dfc426d23566e356d4a38e3209f20b52b9e77427
Seth Etter <sethetter@gmail.com>
2016-11-14 22:00:38 -0600

    Set up initial page content

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Call Yo Rep' });
+});
+
+router.post('/dothething', (req, res, next) => {
+  // TODO: use address + scope to get rep info
+  // TODO: pass along the concern to the template, preformat msg there
 });
 
 module.exports = router;
